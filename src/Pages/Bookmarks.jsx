c983dcd5c7f472dc6cf5356e10f26d6246cd3d22
kotesh-arya <koteswarraomudila@gmail.com>
2022-09-14 00:31:05 +0530

    FEAT: Added Bookmarks Page UI

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -1,23 +1,55 @@
 import React from "react";
-import { Box, Flex, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+  Icon,
+} from "@chakra-ui/react";
+import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 import { Navbar } from "../Components/Navbar";
 import { Sidebar } from "../Components/Sidebar";
+import { Suggestionbar } from "../Components/Suggestionbar";
+import { PostCard } from "../Components/PostCard";
 function Bookmarks() {
+  const btnBg = useColorModeValue("gray.300", "gray.900");
+
   return (
     <div>
+      <Sidebar />
       <Navbar />
       <Flex
-        // bg={"green"}
-        justifyContent={"space-between"}
+        width={"90%"}
+        marginRight="auto"
+        //  bg={"red"}
+        justifyContent={"center"}
         padding={"80px 20px"}
       >
-        <Sidebar />
-        <VStack bg="tomato" width={"40rem"} height="80px">
-          ji
+        <VStack width={"40rem"} height="80px" spacing={12}>
+          <Flex
+            justifyContent={"space-between"}
+            position={"fixed"}
+            bg="whiteAplha.100"
+            zIndex={"99"}
+            width="20%"
+          >
+            <Button bg={btnBg}>
+              {" "}
+              <Icon as={AiOutlineArrowUp} marginRight="2px" />
+              Newest
+            </Button>
+            <Button bg={btnBg}>
+              <Icon as={AiOutlineArrowDown} marginRight="2px" />
+              Oldest
+            </Button>
+          </Flex>
+          <PostCard />
+          <PostCard />
         </VStack>
-        <Box bg="tomato" width={"20rem"} height="80px">
-          bu
-        </Box>
+        <Suggestionbar />
       </Flex>
     </div>
   );
