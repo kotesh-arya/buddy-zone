2bcad41f41dec8f2dd1ec2936bd1c0bd4bd84a96
kotesh-arya <koteswarraomudila@gmail.com>
2022-09-14 00:22:32 +0530

    FEAT: Added Explore page UI

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -1,23 +1,65 @@
 import React from "react";
-import { Box, Flex, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+  Icon,
+} from "@chakra-ui/react";
+import { AiFillFire } from "react-icons/ai";
+import { BiNews, BiChip } from "react-icons/bi";
+import { RiShirtFill } from "react-icons/ri";
 import { Navbar } from "../Components/Navbar";
 import { Sidebar } from "../Components/Sidebar";
+import { Suggestionbar } from "../Components/Suggestionbar";
+import { PostCard } from "../Components/PostCard";
 function Explore() {
+  const btnBg = useColorModeValue("gray.300", "gray.900");
+
   return (
     <div>
+      <Sidebar />
       <Navbar />
       <Flex
-        // bg={"green"}
-        justifyContent={"space-between"}
+        width={"90%"}
+        marginRight="auto"
+        //  bg={"red"}
+        justifyContent={"center"}
         padding={"80px 20px"}
       >
-        <Sidebar />
-        <VStack bg="tomato" width={"40rem"} height="80px">
-          ji
+        <VStack width={"40rem"} height="80px" spacing={12}>
+          <Flex
+            justifyContent={"space-between"}
+            position={"fixed"}
+            bg="whiteAplha.100"
+            zIndex={"99"}
+            width="40%"
+          >
+            <Button bg={btnBg}>
+              {" "}
+              <Icon as={AiFillFire} marginRight="2px" />
+              Trending
+            </Button>
+            <Button bg={btnBg}>
+              <Icon as={BiNews} marginRight="2px" />
+              News
+            </Button>
+            <Button bg={btnBg}>
+              <Icon as={BiChip} marginRight="2px" />
+              Tech
+            </Button>
+            <Button bg={btnBg}>
+              <Icon as={RiShirtFill} marginRight="2px" />
+              Fashion
+            </Button>
+          </Flex>
+          <PostCard />
+          <PostCard />
         </VStack>
-        <Box bg="tomato" width={"20rem"} height="80px">
-          bu
-        </Box>
+        <Suggestionbar />
       </Flex>
     </div>
   );
