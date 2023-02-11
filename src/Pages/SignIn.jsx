5ccd0b9b8c0158d9a17de9b3b48b5e86326a04c9
kotesh-arya <koteswarraomudila@gmail.com>
2023-02-11 23:38:15 +0530

    STYLES: Added responsiveness for signin-page

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import {
   Container,
   Flex,
-  VStack,
   Heading,
   Text,
   FormControl,
@@ -17,7 +16,7 @@ import Logo from "../assets/buddy-zone-blue.png";
 import { Image } from "@chakra-ui/react";
 import { Navbar } from "../Components/Navbar";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logIn } from "../features/auth/authSlice";
 import { USER_DATA, USER_TOKEN } from "../constants";
 
@@ -60,119 +59,119 @@ function SignIn() {
   };
 
   return (
-    <Box backgroundColor={bgColor}>
+    <Box
+      backgroundColor={bgColor}
+      h="100vh"
+      display={"flex"}
+      flexDirection={"row"}
+      justifyContent="center"
+      alignItems={"start"}
+    >
       <Navbar />
-      <Container maxW="container.xl" p={0}>
-        <Flex
-          h="100vh"
-          paddingTop={"4rem"}
-          justifyContent="center"
+      <Container
+        maxW="container.xl"
+        marginTop={{ base: "0rem", md: "0rem", lg: "8rem" }}
+        padding={{ base: "0rem", md: "5rem", lg: "2rem" }}
+        h={{ base: "100vh", md: "100vh", lg: "40rem" }}
+        display={"flex"}
+        flexDirection={{ base: "column", md: "row", lg: null }}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Box
+          flexDirection={"column"}
           alignItems={"center"}
+          display={{ base: "none", md: "flex", lg: null }}
         >
-          <VStack
-            borderRadius={20}
-            h="full"
-            py={10}
-            display={"flex"}
-            flexDirection={"column"}
-            justifyContent={"center"}
-          >
+          <Image
+            src={Logo}
+            w={{ base: "15rem", md: "25rem", lg: "34rem" }}
+            objectFit="contain"
+            alt="Buddy-zone-logo"
+          />
+          <Text as="strong" fontSize={20}>
+            Only Zone for the friendly Ones
+          </Text>
+        </Box>
+        <Flex
+          borderRadius={20}
+          boxShadow="2xl"
+          border={"1px solid grey"}
+          w={{ base: "20rem", md: "25rem", lg: "28rem" }}
+          display={"flex"}
+          padding={"1rem 2rem"}
+          flexDirection={"column"}
+        >
+          <Heading size="xl" marginX={"auto"} marginY="3">
+            Signin
+          </Heading>
+          <form onSubmit={(e) => e.preventDefault()}>
             <Box
+              alignItems={"center"}
               display={"flex"}
               flexDirection={"column"}
-              alignItems={"center"}
+              width="100%"
             >
-              <Image
-                src={Logo}
-                width="600px"
-                objectFit="contain"
-                alt="Buddy-zone-logo"
-              />
-              <Text as="strong" fontSize={20}>
-                One & Only Zone for the friendly Ones
-              </Text>
-            </Box>
-          </VStack>
-          <Flex
-            borderRadius={20}
-            boxShadow="2xl"
-            border={"1px solid gray"}
-            w="28rem"
-            display={"flex"}
-            padding={"1rem 2rem"}
-            flexDirection={"column"}
-          >
-            <Heading size="xl" marginX={"auto"} marginY="3">
-              Signin
-            </Heading>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <Box
-                alignItems={"center"}
-                display={"flex"}
-                flexDirection={"column"}
-                width="100%"
-              >
-                <FormControl>
-                  <FormLabel>Username</FormLabel>
-                  <Input
-                    type={"text"}
-                    name="username"
-                    value={loginUser.username}
-                    onChange={(e) => userInputHandler(e)}
-                    placeholder="kotesharya@gmail.com"
-                  />
-                </FormControl>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type={"text"}
+                  name="username"
+                  value={loginUser.username}
+                  onChange={(e) => userInputHandler(e)}
+                  placeholder="kotesharya@gmail.com"
+                />
+              </FormControl>
 
-                <FormControl>
-                  <FormLabel>Password</FormLabel>
-                  <Input
-                    type={"password"}
-                    name="password"
-                    value={loginUser.password}
-                    onChange={(e) => userInputHandler(e)}
-                    placeholder="********"
-                  />
-                </FormControl>
+              <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type={"password"}
+                  name="password"
+                  value={loginUser.password}
+                  onChange={(e) => userInputHandler(e)}
+                  placeholder="********"
+                />
+              </FormControl>
 
-                <Box width={"100%"} padding={"1rem 0"} marginTop={"2rem"}>
-                  <Button
-                    bg={"#08a0e9"}
-                    color="white"
-                    width={"100%"}
-                    marginBottom={"1rem"}
-                    onClick={() => {
-                      loginHandler(loginUser);
-                    }}
-                  >
-                    SignIn
-                  </Button>
+              <Box width={"100%"} padding={"1rem 0"} marginTop={"2rem"}>
+                <Button
+                  bg={"#08a0e9"}
+                  color="white"
+                  width={"100%"}
+                  marginBottom={"1rem"}
+                  onClick={() => {
+                    loginHandler(loginUser);
+                  }}
+                >
+                  SignIn
+                </Button>
 
-                  <Button
-                    as={Link}
-                    to="/signup"
-                    outline={"1px #08a0e9 "}
-                    variant="outline"
-                    color="#08a0e9"
-                    width={"100%"}
-                    marginBottom={"1rem"}
-                  >
-                    Create Account
-                  </Button>
-                  <Button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      loginHandler(tester);
-                      navigate("/home");
-                    }}
-                    bg="#08a0e9"
-                    width={"100%"}
-                  >
-                    Guest Login
-                  </Button>
-                </Box>
+                <Button
+                  as={Link}
+                  to="/signup"
+                  outline={"1px #08a0e9 "}
+                  variant="outline"
+                  color="#08a0e9"
+                  width={"100%"}
+                  marginBottom={"1rem"}
+                >
+                  Create Account
+                </Button>
+                <Button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    loginHandler(tester);
+                    navigate("/home");
+                  }}
+                  bg="#08a0e9"
+                  width={"100%"}
+                >
+                  Guest Login
+                </Button>
               </Box>
-            </form>
-          </Flex>
+            </Box>
+          </form>
         </Flex>
       </Container>
     </Box>
