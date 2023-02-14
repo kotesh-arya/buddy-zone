8b781cb2b489fb306ce8fd6fe6a2882d88c8a9c9
kotesh-arya <koteswarraomudila@gmail.com>
2023-02-14 11:05:42 +0530

    STYLES: Responsive bookmarks page

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -14,7 +14,7 @@ import { Sidebar } from "../Components/Sidebar";
 import { Suggestionbar } from "../Components/Suggestionbar";
 import { PostCard } from "../Components/PostCard";
 import { useSelector } from "react-redux";
-
+import { BottomNavigation } from "../Components/BottomNavigation";
 function Bookmarks() {
   const btnBg = useColorModeValue("gray.300", "gray.900");
   const { bookmarks } = useSelector((store) => store.bookmark);
@@ -22,19 +22,40 @@ function Bookmarks() {
     <Box>
       <Sidebar />
       <Navbar />
-      <Flex
-        width={"90%"}
-        marginRight="auto"
-        justifyContent={"center"}
-        padding={"80px 20px"}
+      <Box
+        width={"100%"}
+        display={"flex"}
+        flexDirection={{
+          base: "column",
+          md: "column",
+          lg: "column",
+          xl: "row",
+        }}
+        alignItems={{
+          base: "center",
+          md: "flex-end",
+          lg: "flex-end",
+          xl: "flex-start",
+        }}
+        justifyContent={{
+          base: "center",
+          md: "center",
+          lg: "center",
+          xl: "flex-end",
+        }}
+        padding={"80px 0px"}
+        paddingRight={{ md: "0rem", lg: "1.5rem", xl: "0rem" }}
       >
-        <VStack width={"40rem"} height="80px" spacing={12}>
+        <VStack
+          spacing={12}
+          marginRight={{ base: "0rem", md: "1rem", lg: "6rem" }}
+        >
           <Flex
             justifyContent={"space-between"}
             position={"fixed"}
             bg="whiteAplha.100"
             zIndex={"99"}
-            width="20%"
+            width={{ base: "90%", md: "60%", lg: "40%" }}
           >
             <Button bg={btnBg}>
               <Icon as={AiOutlineArrowUp} marginRight="2px" />
@@ -45,16 +66,19 @@ function Bookmarks() {
               Oldest
             </Button>
           </Flex>
-          {bookmarks.length > 0 ? (
-            bookmarks.map((post) => {
-              return <PostCard key={post._id} {...post} />;
-            })
-          ) : (
-            <Heading>You have not bookmarked any post yet!</Heading>
-          )}
+          <Box>
+            {bookmarks.length > 0 ? (
+              bookmarks.map((post) => {
+                return <PostCard key={post._id} {...post} />;
+              })
+            ) : (
+              <Heading>You have not bookmarked any post yet!</Heading>
+            )}
+          </Box>
         </VStack>
         <Suggestionbar />
-      </Flex>
+      </Box>
+      <BottomNavigation />
     </Box>
   );
 }
